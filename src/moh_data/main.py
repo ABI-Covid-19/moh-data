5eb9498425c784aba32c595a749736673210a01a
mahyar-osn <m.osanlouy@auckland.ac.nz>
2020-04-07 16:20:36 +1200

    Updated main to account for new changes.

--- src/moh_data/main.py
+++ src/moh_data/main.py
@@ -1,6 +1,5 @@
-from .core.collector import DataCollector
-
-import matplotlib.pyplot as plt
+from src.moh_data.core.collector import DataCollector
+from src.moh_data.utils.visualisation import Visualisation
 
 
 class Basic:
@@ -14,6 +13,8 @@ class Basic:
         self._total_combined = None
         self._grand_sum = None
 
+        self._vis = Visualisation()
+
         self._run()
 
     def _run(self):
@@ -21,18 +22,22 @@ class Basic:
         self._probable = self._excel_file.parse_probable()
         self._total_daily_confirmed = self._excel_file.get_daily_sum_confirmed()
         self._total_daily_probable = self._excel_file.get_daily_sum_probable()
-        self._total_combined, self._grand_sum = self._excel_file.generate_combined_sum()
+        self._total_combined = self._excel_file.get_cumulative_sum()
+        self._grand_sum = self._excel_file.get_grand_sum()
+        self._total_arrival = self._excel_file.get_cumulative_arrival_sum()
 
     def plot_daily_trend(self):
-        self._total_combined.plot()
+        self._vis.set_data(self._total_combined, tick_interval=(2.0, 5.0))
 
     def plot_cumulative_sum(self):
-        self._grand_sum.plot()
+        self._vis.set_data(self._grand_sum, tick_interval=(2.0, 100.0))
+
+    def plot_cumulative_arrival_sum(self):
+        self._vis.set_data(self._total_arrival, tick_interval=(2.0, 5.0))
 
 
 if __name__ == '__main__':
     run_data = Basic()
     run_data.plot_daily_trend()
     run_data.plot_cumulative_sum()
-
-    plt.show()
+    run_data.plot_cumulative_arrival_sum()
