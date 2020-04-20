9a2b2038f9566f4e960e8f9fd032f6f5dcd79e31
Alan Garny <agarny@hellix.com>
2020-04-20 16:42:38 +1200

    Some minor cleaning up.

--- src/moh_data/main.py
+++ src/moh_data/main.py
@@ -247,9 +247,9 @@ if __name__ == '__main__':
         run_data.plot_daily_arrival_sum()
         run_data.plot_overseas_date_reported()
 
-        print('Cumulative confirmed cases on day 41 = ', run_data.get_cumulative_confirmed_cases_on_day(40))
-        print('Cumulative probable cases on day 41 = ', run_data.get_cumulative_probable_cases_on_day(40))
-        print('Cumulative total cases on day 41 = ', run_data.get_cumulative_total_cases_on_day(40))
+        print('Cumulative confirmed cases on day 40 = ', run_data.get_cumulative_confirmed_cases_on_day(40))
+        print('Cumulative probable cases on day 40 = ', run_data.get_cumulative_probable_cases_on_day(40))
+        print('Cumulative total cases on day 40 = ', run_data.get_cumulative_total_cases_on_day(40))
         print('Cumulative recovered cases on day 80 = ', run_data.get_cumulative_recovered_cases_on(80))
         print('Cumulative dead cases on day 80 = ', run_data.get_cumulative_dead_cases_on(80))
 
